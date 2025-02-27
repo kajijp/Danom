a990d61b28afd2e423f9a530d4eb21ec64197177
Danom <danomsite@gmail.com>
2025-02-27 15:12:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 clear
 sudo apt update && sudo apt upgrade -y
 mkdir ~/Danom && cd ~/Danom
-wget https://github.com/DanomSite/release/releases/download/v1/danom_v1.tar.gz && tar -xvzf danom_v1.tar.gz && ./danom
+wget https://github.com/DanomSite/release/releases/download/v1/danom_v1.tar.gz && tar -xvzf danom_v1.tar.gz
