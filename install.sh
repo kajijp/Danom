680c5af552f66e9cf66ce10b383fbad2240f5227
Danom <danomsite@gmail.com>
2025-03-01 00:26:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
-wget https://github.com/DanomSite/release/releases/download/v2/DanomV2.tar.gz
-tar -xvzf DanomV2.tar.gz
+wget https://github.com/DanomSite/release/releases/download/v3/DanomV3.tar.gz
+tar -xvzf DanomV3.tar.gz
